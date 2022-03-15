392e417dcafaa57b4a4f8afbf94244cd7773a594
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-15 23:48:37 +0530

    read documents from mongodb database

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,11 @@
+// CRUD Create Read Update Delete
+
 const { MongoClient, ObjectID, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectId()
-console.log(id.id.length)
-console.log(id.toHexString().length)
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -14,50 +14,23 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     
     const db = client.db(databaseName)
 
-//     db.collection('users').insertOne({
-//         _id: id,
-//         name: 'Shekhar',
-//         age: 23
-//     }, (error, result) => {
-//         if (error) {
-//             return console.log('Unable to insert user')
-//         }
+    // db.collection('users').findOne({ _id: new ObjectID("6230a568fddd5315862ec38f") }, (error, user) => {
+    //     if(error) {
+    //         return console.log('Unable to fetch')
+    //     }
 
-//         console.log(result.ops)
-//     })
-// })
+    //     console.log(user)
+    // })
 
-// db.collection('users').insertMany([
-//     {
-//         name: 'John',
-//         age: 30
-//     }, {
-//         name: 'Snow',
-//         age: 27
-//     }
-// ], (error, result) => {
-//     if (error) {
-//         return console.log('Unable to insert documents!')
-//     }
+    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+    
+    db.collection('tasks').findOne({ _id: new ObjectId("6230b13e0a64cbd1a5c279f4") }, (error, task) => {
+        console.log(task)
+    })
 
-//     console.log(result.ops)
-//   })
-
-// db.collection('tasks').insertMany([
-//     {
-//         description: 'Clean the house',
-//         completed: true
-//     },{
-//         description: 'Renew inspection',
-//         completed: false
-//     }, {
-//         description: 'Pot plants',
-//         completed: false
-//     }
-// ], (error, result) => {
-//     if (error) {
-//         return console.log('Unable to insert tasks!')
-//     }
-//     console.log(result.ops)
-//   })
+    db.collection('tasks').find({ completed: false}).toArray((error, tasks) => {
+        console.log(tasks)
+    })
 })
