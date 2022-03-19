21cad8d0aa2fae66a44f5e202d6ccf38307f2845
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-20 02:04:11 +0530

    resource deleting endpoints

--- src/index.js
+++ src/index.js
@@ -66,6 +66,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -125,6 +139,19 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
