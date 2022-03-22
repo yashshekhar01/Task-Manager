cdfdd3d51e552f0c1fa559f90db450be276cf93f
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-23 00:48:09 +0530

    using JSON web tokens

--- src/index.js
+++ src/index.js
@@ -14,18 +14,14 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-    const password = 'Blue12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
+   const token = jwt.sign({ _id: 'abc123' }, 'thisisanodecourse', { expiresIn: '7 days' })
+   console.log(token)
 
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('Blue12345!', hashedPassword)
-    console.log(isMatch)
-    
+   const data = jwt.verify(token, 'thisisanodecourse')
+   console.log(data)
 }
 
 myFunction()
