c2fd2da2554a6ba9e08518e8841049d0966ae7f3
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-27 17:11:06 +0530

    hiding private data

--- src/index.js
+++ src/index.js
@@ -26,14 +26,6 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
 
-const myFunction = async () => {
-   const token = jwt.sign({ _id: 'abc123' }, 'thisisanodecourse', { expiresIn: '7 days' })
-   console.log(token)
 
-   const data = jwt.verify(token, 'thisisanodecourse')
-   console.log(data)
-}
 
-myFunction()
