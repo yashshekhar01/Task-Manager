8c8c822b24d646de760d04fca22163b4dc65215b
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-27 16:49:21 +0530

    logging out

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { status } = require('express/lib/response')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const res = require('express/lib/response')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -26,6 +27,29 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
@@ -37,7 +61,7 @@ router.get('/users/:id', async (req, res) => {
         const user = await User.findById(_id)
 
         if (!user) {
-            return res.status(404).send()
+            return res.status(404).send()  
         }
 
         res.send(user)
